21306c5a240c2e48e6f9feb89ee6da7939f06719
Crindzebra <crs_16423@outlook.com>
2019-01-10 12:45:31 +0800

    add

--- hxtest.js
+++ hxtest.js
@@ -3,11 +3,25 @@ class HxTest extends HTMLElement{
         super();
         const shadow=this.attachShadow({mode:"open"});
         let para=document.createElement("p");
+        let attrPara=document.createElement("p");
+        attrPara.className="attr-para"
+        attrPara.textContent=`Attr 'test' is ${this.getAttribute("test")}`;
         para.textContent="Hello World!";
         shadow.appendChild(para);
+        shadow.appendChild(attrPara);
+        let testSlot=document.createElement("slot");
+        testSlot.setAttribute("name","test");
+        shadow.appendChild(testSlot);
     }
     connectedCallback(){
-        alert("test");
+        console.log("hxtest connected");
+    }
+    attributeChangedCallback(name, oldValue, newValue){
+        console.log(`hxtest attr "${name}" has changed from "${oldValue}" to "${newValue}"`);
+        const shadow=this.attachShadow({mode:"open"});
+        const attrPara=shadow.querySelector(".attr-para");
+        if(name=="test")
+            attrPara.textContent=`Attr 'test' is ${newValue}`; 
     }
 }
 customElements.define("hx-test",HxTest);
