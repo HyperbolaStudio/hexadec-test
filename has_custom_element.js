aa98bbbf7ab194c8aa17dbafdcd8303ffae899c4
Crindzebra <crs_16423@outlook.com>
2019-01-11 15:35:35 +0800

    add exam

---
+++ has_custom_element.js
@@ -0,0 +1,12 @@
+if(!window.customElements){
+    alert(`
+当前浏览器不支持Custom Elements，可能无法按照预期效果展示。请使用支持Custom Elements的最新浏览器访问。
+
+
+每去适配一个古老浏览器，就会有至少一名程序员绞尽脑汁工作甚至不惜熬夜加班
+而程序员每一次加班，都增加了自己猝死的风险
+救救程序员！
+——techmoe的回答 - 知乎
+
+    `);
+}
